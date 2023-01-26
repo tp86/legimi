41e7611247237f9bfc4d6e6181c5fd005c26c6a6
tp86 <tom0palka+tp86@gmail.com>
2023-01-26 15:23:56 +0100

    Add libs to package.path

--- setup.lua
+++ setup.lua
@@ -1,1 +1,6 @@
-package.path = table.concat({ "src/?.lua", ".luarocks/share/lua/5.4/?.lua", package.path }, ";")
+package.path = table.concat({
+  "src/?.lua",
+  "lib/?/src/?.lua",
+  ".luarocks/share/lua/5.4/?.lua",
+  package.path
+}, ";")
