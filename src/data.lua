5af875ba134c31bdcc2ce0a81d86a39a9ca1c532
tp86 <tom0palka+tp86@gmail.com>
2023-01-28 21:34:32 +0000

    Simplify serializers

--- src/data.lua
+++ src/data.lua
@@ -1,37 +1,19 @@
 local class = require "class"
-local init = class.constructor
-
-local function passthroughinit(self, ...)
-  class.parent(self)(...)
-end
-
-local Number = class {
-  formatprefix = "i",
-  [init] = function(self, value)
-    self.value = value
-    self.format = self.formatprefix .. self.size
-  end,
-  get = function(self)
-    return self.value
-  end,
-  set = function(self, value)
-    self.value = value
-  end,
-  pack = function(self)
-    return string.pack(self.format, self.value)
-  end,
-  unpack = function(self, data)
-    self.value = string.unpack(self.format, data)
-  end,
-  length = function(self)
-    return self.size
-  end,
-}
 
 local function makenumberclass(size)
-  return class.extends(Number) {
-    size = size,
-    [init] = passthroughinit,
+  local formatprefix = "i"
+  local format = formatprefix .. size
+  return class {
+    format = format,
+    pack = function(value)
+      return string.pack(format, value)
+    end,
+    unpack = function(data)
+      return string.unpack(format, data)
+    end,
+    length = function()
+      return size
+    end,
   }
 end
 
@@ -42,32 +24,21 @@ local numbers = {
   long = makenumberclass(8),
 }
 
-local Length = class {
-  formatprefix = "I",
-  lengthsize = 4,
-  [init] = function(self)
-    self.format = self.formatprefix .. self.lengthsize .. self.format
-  end,
-  pack = function(self)
-    return string.pack(self.format, self.size, self:get())
-  end,
-  unpack = function(self, data)
-    local _, value = string.unpack(self.format, data)
-    self:set(value)
-  end,
-  length = function(self)
-    return self.size + self.lengthsize
-  end,
-}
-
 local function extendwithlength(numberclass)
-  local function initfn(self, value)
-    class.parent(self, numberclass)(value)
-    class.parent(self, Length)()
-  end
-
-  return class.extends(Length, numberclass) {
-    [init] = initfn,
+  local formatprefix = "I"
+  local lengthsize = 4
+  local format = formatprefix .. lengthsize .. numberclass.format
+  return class.extends(numberclass) {
+    pack = function(value)
+      return string.pack(format, numberclass.length(), value)
+    end,
+    unpack = function(data)
+      local _, value = string.unpack(format, data)
+      return value
+    end,
+    length = function()
+      return numberclass.length() + lengthsize
+    end,
   }
 end
 
@@ -78,61 +49,35 @@ local numberswithlength = {
   long = extendwithlength(numbers.long),
 }
 
-local Multivalued = class {
-  [init] = function(self, values)
-    self:set(values)
-  end,
-  set = function(self, values)
-    for i, value in pairs(values or {}) do
-      if not self.values then self.values = {} end
-      self.values[i] = value
-    end
-  end,
-  get = function(self)
-    return self.values
-  end,
-}
-
-local GenericSequence = class.extends(Multivalued) {
-  [init] = passthroughinit,
-  pack = function(self)
-    local serialized = {}
-    for i, datatype in ipairs(self.types) do
-      local value = self.values[i]
-      serialized[i] = datatype(value):pack()
-    end
-    return table.concat(serialized)
-  end,
-  unpack = function(self, data)
-    local values = {}
-    for i, datatype in ipairs(self.types) do
-      local dataobject = datatype()
-      dataobject:unpack(data)
-      values[i] = dataobject:get()
-      data = data:sub(dataobject:length() + 1)
-    end
-    self.values = values
-  end,
-  length = function(self)
-    local totallength = 0
-    for _, datatype in ipairs(self.types) do
-      totallength = totallength + datatype():length()
-    end
-    return totallength
-  end,
-}
-
-local Sequence = function(types)
-  return class.extends(GenericSequence) {
-    types = types,
-    [init] = passthroughinit,
+local Sequence = function(serializers)
+  return class {
+    pack = function(values)
+      local serialized = {}
+      for i, serializer in ipairs(serializers) do
+        local value = values[i]
+        serialized[i] = serializer.pack(value)
+      end
+      return table.concat(serialized)
+    end,
+    unpack = function(data)
+      local values = {}
+      for i, serializer in ipairs(serializers) do
+        values[i] = serializer.unpack(data)
+        data = data:sub(serializer.length() + 1)
+      end
+      return values
+    end,
+    length = function()
+      local totallength = 0
+      for _, serializer in ipairs(serializers) do
+        totallength = totallength + serializer.length()
+      end
+      return totallength
+    end,
   }
 end
 
 local Array = function()
-  return class.extends(Multivalued) {
-    [init] = passthroughinit,
-  }
 end
 
 return {
--- test/data.lua
+++ test/data.lua
@@ -3,102 +3,68 @@ local lu = require "luaunit"
 local data = require "data"
 
 local function testnumber(what)
-  local description = what.description
-  local dataclass = what.class
+  local classname = what.class
   local serialized = what.serialized
   local deserialized = what.deserialized
 
-  _ENV["Test_" .. description] = {
+  _ENV["Test_" .. classname] = {
 
-    test_can_be_created_without_value = function()
-      local object = dataclass()
-      lu.assert_not_nil(object)
-      lu.assert_nil(object:get())
+    test_can_serialize_value = function()
+      local serializer = data[classname]
+      lu.assert_equals(serializer.pack(deserialized), serialized)
     end,
 
-    test_can_be_created_with_value = function()
-      local value = 1
-      local object = dataclass(value)
-      lu.assert_equals(object:get(), value)
-    end,
-
-    test_can_be_set_after_creation = function()
-      local object = dataclass()
-      lu.assert_nil(object.value)
-      local value = 2
-      object:set(value)
-      lu.assert_equals(object:get(), value)
-    end,
-
-    test_can_be_serialized = function()
-      local object = dataclass(deserialized)
-      local expected = serialized
-      local actual = object:pack()
-      lu.assert_equals(actual, expected)
-    end,
-
-    test_can_be_deserialized = function()
-      local object = dataclass()
-      local value = serialized
-      local expected = deserialized
-      object:unpack(value)
-      lu.assert_equals(object:get(), expected)
+    test_can_deserialize_value = function()
+      local serializer = data[classname]
+      lu.assert_equals(serializer.unpack(serialized), deserialized)
     end,
   }
 end
 
 testnumber {
-  description = "byte",
-  class = data.Byte,
+  class = "Byte",
   serialized = "\x03",
   deserialized = 3,
 }
 
 testnumber {
-  description = "short",
-  class = data.Short,
+  class = "Short",
   serialized = "\x03\x04",
   deserialized = 0x0403,
 }
 
 testnumber {
-  description = "int",
-  class = data.Int,
+  class = "Int",
   serialized = "\x03\x04\x05\x06",
   deserialized = 0x06050403,
 }
 
 testnumber {
-  description = "long",
-  class = data.Long,
+  class = "Long",
   serialized = "\x03\x04\x05\x06\x07\x08\x09\x0a",
   deserialized = 0x0a09080706050403,
 }
 
 testnumber {
-  description = "byte_with_length",
-  class = data.LenByte,
+  class = "LenByte",
   serialized = "\x01\x00\x00\x00\x03",
   deserialized = 3,
 }
 
 testnumber {
-  description = "short_with_length",
-  class = data.LenShort,
+  class = "LenShort",
   serialized = "\x02\x00\x00\x00\x03\x04",
   deserialized = 0x0403,
 }
 
 testnumber {
-  description = "int_with_length",
-  class = data.LenInt,
+  class = "LenInt",
   serialized = "\x04\x00\x00\x00\x03\x04\x05\x06",
   deserialized = 0x06050403,
 }
 
 testnumber {
-  description = "long_with_length",
-  class = data.LenLong,
+  class = "LenLong",
   serialized = "\x08\x00\x00\x00\x03\x04\x05\x06\x07\x08\x09\x0a",
   deserialized = 0x0a09080706050403,
 }
@@ -107,39 +73,19 @@ local Seq = data.Sequence
 
 Test_sequence = {
 
-  test_can_be_created_with_types_but_without_values = function()
-    local seq = Seq { data.Byte, data.LenLong } ()
-    lu.assert_not_nil(seq)
-    lu.assert_nil(seq:get())
-  end,
-
-  test_can_be_created_with_values = function()
-    local seq = Seq { data.Byte, data.LenInt } { 1, 2 }
-    local expected = { 1, 2 }
-    lu.assert_equals(seq:get(), expected)
-  end,
-
-  test_can_have_values_set_after_creating = function()
-    local seq = Seq { data.Short, data.Int, data.Byte } ()
-    lu.assert_nil(seq:get())
-    local values = { 3, nil, 4 }
-    seq:set(values)
-    lu.assert_equals(seq:get(), values)
-  end,
-
-  test_can_be_serialized = function()
-    local seq = Seq { data.Byte, data.LenShort } { 5, 6 }
+  test_can_serialize_values = function()
+    local seq = Seq { data.Byte, data.LenShort }
     local expected = "\x05\x02\x00\x00\x00\x06\x00"
-    local actual = seq:pack()
+    local actual = seq.pack({ 5, 6 })
     lu.assert_equals(actual, expected)
   end,
 
-  test_can_be_deserialized = function()
-    local seq = Seq { data.LenByte, data.Short } ()
+  test_can_deserialize_values = function()
+    local seq = Seq { data.LenByte, data.Short }
     local value = "\x01\x00\x00\x00\x07\x08\x00"
     local expected = { 7, 8 }
-    seq:unpack(value)
-    lu.assert_equals(seq:get(), expected)
+    local actual = seq.unpack(value)
+    lu.assert_equals(actual, expected)
   end,
 }
 
@@ -147,25 +93,19 @@ local nested = Seq { data.Byte, data.Byte }
 
 Test_sequence_nested = {
 
-  test_can_be_created = function()
-    local seq = Seq { nested } { { 7, 8 } }
-    lu.assert_not_nil(seq)
-    lu.assert_equals(seq:get(), { { 7, 8 } })
-  end,
-
-  test_can_be_serialized = function()
-    local seq = Seq { nested, data.Short } { { 8, 9 }, 10 }
+  test_can_serialize_values = function()
+    local seq = Seq { nested, data.Short }
     local expected = "\x08\x09\x0a\x00"
-    local actual = seq:pack()
+    local actual = seq.pack({ { 8, 9 }, 10 })
     lu.assert_equals(actual, expected)
   end,
 
-  test_can_be_deserialized = function()
-    local seq = Seq { data.Byte, nested, data.Byte } ()
+  test_can_deserialize_values = function()
+    local seq = Seq { data.Byte, nested, data.Byte }
     local value = "\x0b\x0c\x0d\x0e"
     local expected = { 11, { 12, 13 }, 14 }
-    seq:unpack(value)
-    lu.assert_equals(seq:get(), expected)
+    local actual = seq.unpack(value)
+    lu.assert_equals(actual, expected)
   end,
 }
 
@@ -173,24 +113,6 @@ local Arr = data.Array
 
 Test_array = {
 
-  test_can_be_created_with_type_but_without_values = function()
-    local arr = Arr(data.Byte)()
-    lu.assert_not_nil(arr)
-    lu.assert_nil(arr:get())
-  end,
-
-  test_can_be_created_with_values = function()
-    local arr = Arr(data.Byte) { 1, 2, 3 }
-    lu.assert_equals(arr:get(), { 1, 2, 3 })
-  end,
-
-  test_can_have_values_set_after_creating = function()
-    local arr = Arr(data.Short)()
-    lu.assert_nil(arr:get())
-    local values = { 4, 5, 6 }
-    arr:set(values)
-    lu.assert_equals(arr:get(), values)
-  end,
 }
 
 local runner = not ... or #arg > 0
