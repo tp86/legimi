26347221c8c47a057803aff43bf0bf886741e6c9
tp86 <tom0palka+tp86@gmail.com>
2023-02-01 13:11:15 +0100

    Serialize packet

--- src/packet/init.lua
+++ src/packet/init.lua
@@ -1,8 +1,22 @@
 local class = require "class"
 local init = class.constructor
+local ser = require "serializer"
+
+local packetversion = 17
 
 local Packet = class {
-  [init] = function() end,
+  serializer = ser.Sequence { ser.RawInt, ser.RawShort, ser.Str },
+  [init] = function(self, type, content)
+    self.type = type
+    self.content = content
+  end,
+  pack = function(self)
+    return self.serializer.pack({ packetversion, self.type, self.content })
+  end,
+  unpack = function(cls, data)
+    local unpacked = cls.serializer.unpack(data)
+    return cls(unpacked[2], unpacked[3])
+  end,
 }
 
 return {
--- test/packet.lua
+++ test/packet.lua
@@ -8,6 +8,26 @@ Test_packet = {
     local packet = Packet()
     lu.assert_not_nil(packet)
   end,
+
+  test_can_be_serialized = function()
+    local type = -1
+    local content = ""
+    local packet = Packet(type, content)
+    local expected = "\x11\x00\x00\x00\xff\xff\x00\x00\x00\x00"
+    local actual = packet:pack()
+    lu.assert_equals(actual, expected)
+  end,
+
+  test_can_be_deserialized = function()
+    local data = "\x11\x00\x00\x00\xff\xff\x00\x00\x00\x00"
+    local expected = {
+      type = -1,
+      content = "",
+    }
+    local actual = Packet:unpack(data)
+    lu.assert_equals(actual.type, expected.type)
+    lu.assert_equals(actual.content, expected.content)
+  end,
 }
 
 local runner = not ... or #arg > 0
